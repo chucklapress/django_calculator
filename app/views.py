97c0924f700994a80a983dee76ca02b7c4329fd3
chucklapress <charleslapress@gmail.com>
2016-06-15 17:29:37 -0400

    changes

--- app/views.py
+++ app/views.py
@@ -4,7 +4,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    print(request.GET)
+    if request.POST:
+        left = request.POST['left']
+        right = request.POST['right']
+        mathop =request.POST['operators']
+        if mathop == 'add':
+            value = int(left) + int(right)
+        if mathop == 'subtract':
+            value = int(left) - int(right)
+        if mathop == 'multiply':
+            value = int(left) * int(right)
+        if mathop == 'divide':
+            value = int(left) / int(right)
+
+        context ={
+            'total':value
+        }
+        print(request.POST)
+        return render(request, 'index.html', context)
+
+    print(request.POST)
     return render(request, 'index.html')
 
 
